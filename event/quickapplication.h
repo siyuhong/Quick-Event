fe0aa44fd60495d54ffa097f374dd9aee6d0e252
dingda.li <dingda.li@freemud.cn>
2020-04-27 08:58:31 +0800

    1.重构

--- event/quickapplication.h
+++ event/quickapplication.h
@@ -10,10 +10,20 @@
 #include <QVariant>
 #include <QSharedPointer>
 #include <QThread>
+#include <QGenericArgument>
+#include <QtCore>
 
 #include "quickevent.h"
 
 class QuickWork;
+
+enum PublishType {
+    AutoPublish,
+    DirectPublish,
+    QueuedPublish,
+    BlockingQueuedPublish,
+    UniquePublish = 0x80
+};
 
 class QuickApplication : public QApplication
 {
@@ -22,6 +32,44 @@ public:
     QuickApplication(int &argc, char **argv);
 
     virtual ~QuickApplication();
+
+    template<class T>
+    static void getAddress(QList<QGenericArgument> &list, T&& t)
+    {
+        list << Q_ARG(decltype(t), t);
+    }
+
+    template<class ...Args>
+    static void publishEvent(QByteArray eventName, PublishType type, Args&&... args)
+    {
+        if(eventName.isEmpty())
+            return ;
+
+        auot argsNum = sizeof...(args);
+        Q_ASSERT_X(argsNum <= 10, "QuickEvent", "publishEvent argv number not greater than 10");
+
+
+        QList<QGenericArgument> list;
+        int[] arr = {(getAddress(list, args),0)...};
+        auto len = list.size();
+
+        while(len < 10) list << QGenericArgument();
+
+        QReadLocker loker(&s_lock);
+
+        if(s_quick_event_pool.contains(name))
+        {
+            auto set = s_quick_event_pool[name];
+
+            foreach (auto var, set) {
+                auto methodIndex = 2;
+                var->metaObject()->method(methodIndex).invoke(var, type, list[0], list[1], list[2], list[3],
+                                                             list[4],list[5], list[6], list[7], list[8], list[9]);
+            }
+        }
+
+
+    }
 
     static void doPostEvent(QObject *receiver, QEvent *event, int priority = Qt::NormalEventPriority);
 
@@ -62,6 +110,7 @@ public:
     static void logoutEvent(QObject *listener, QByteArray eventName);
 
     static void exit(int rlt = 0);
+
 private:
     static QMap< QByteArray, QSet<QObject *> > s_quick_event_pool;
 
